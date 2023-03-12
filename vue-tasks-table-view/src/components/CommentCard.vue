0b44267aa1c1297873fe2d1405989bc79e258d57
hazadus <hazadus7@gmail.com>
2023-03-12 19:15:50 +0300

    ui(journal/Vue): implemented inline comment editing UI (without actual POST).

--- vue-tasks-table-view/src/components/CommentCard.vue
+++ vue-tasks-table-view/src/components/CommentCard.vue
@@ -10,8 +10,8 @@
         <i class="fa-solid fa-user"></i> {{ useAuthorShortName(comment) }}, {{ useFormatDateTime(comment.created) }}
       </div>
 
-      <!-- Buttons on the right side -->
-      <div>
+      <!-- Buttons on the right side - hide when editing -->
+      <div v-if="!isEditing">
         <!-- Edit button -->
         <a v-if="comment.author === userInfo.id"
            class="toolbar-link toolbar-link-primary me-1"
@@ -27,8 +27,53 @@
       </div>
     </div>
 
-    <!-- Body -->
-    <div class="card-body" :id="'comment-body-' + comment.id">
+    <!-- COMMENT EDITOR STUFF -->
+    <!-- Edit / preview tabs -->
+    <ul v-if="isEditing" class="nav nav-tabs ms-1 mt-1">
+      <li class="nav-item">
+        <a class="nav-link"
+           :class="activeEditorTabName === 'edit' ? 'active' : ''"
+           @click.prevent="activeEditorTabName = 'edit'"
+           href="#">Редактировать</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"
+           :class="activeEditorTabName === 'preview' ? 'active' : ''"
+           @click.prevent="activeEditorTabName = 'preview'"
+           href="#">Предпросмотр</a>
+      </li>
+    </ul>
+
+    <div v-if="isEditing" class="d-flex mb-3 flex-wrap ms-1 me-1">
+      <!-- Comment editor -->
+      <textarea v-if="activeEditorTabName === 'edit'" class="form-control"
+                v-model="modifiedBody"
+                :rows="textAreaRowsQty">
+      </textarea>
+      <!-- Comment preview -->
+      <div v-if="activeEditorTabName === 'preview'" class="card-body d-block w-100">
+        <span v-html="markdownToHtml(this.modifiedBody)"></span>
+      </div>
+
+      <!-- Buttons -->
+      <div class="flex-grow-1">
+        &nbsp;
+        <!-- Add some info text here... -->
+      </div>
+      <div class="btn-toolbar mt-1 mb-2 mb-md-0 justify-content-end">
+        <button class="btn btn-sm btn-secondary me-1"
+                @click="isEditing=false">
+          <i class="fa-solid fa-cancel"></i> Отмена
+        </button>
+        <button class="btn btn-sm btn-success"
+                @click="onClickSave">
+            <i class="fa-solid fa-save"></i> Сохранить
+        </button>
+      </div>
+    </div>
+
+    <!-- COMMENT BODY -->
+    <div v-if="!isEditing" class="card-body" :id="'comment-body-' + comment.id">
       <span v-html="markdownToHtml(comment.body)"></span>
     </div>
 
@@ -36,7 +81,7 @@
 </template>
 
 <script>
-import {useFormatDateTime, useAuthorShortName} from "@/utils";
+import {useFormatDateTime, useAuthorShortName, useLinesCount} from "@/utils";
 
 export default {
   name: "CommentCard",
@@ -47,13 +92,28 @@ export default {
   data() {
     return {
       isEditing: false,
-      modifiedBody: this.comment.body, // Represents comment body content changed while editing.
+      activeEditorTabName: 'edit',  // 'edit' or 'preview' tabs.
+      modifiedBody: null, // Represents comment body content changed while editing.
     }
+  },
+  computed: {
+    textAreaRowsQty() {
+      // Make text area taller to match number of lines in the comment,
+      // using `minRows` as minimal height of the text area.
+      const minRows = 3;
+      const linesQty = useLinesCount(this.modifiedBody);
+      return linesQty <= minRows ? minRows : linesQty;
+    },
   },
   methods: {
     useFormatDateTime,
     useAuthorShortName,
+    useLinesCount,
     onClickEdit() {
+      this.isEditing = true;
+      this.modifiedBody = this.comment.body;
+    },
+    onClickSave() {
       alert('В РАЗРАБОТКЕ!\nЗдесь редактирование пока не работает - воспользуйтесь кнопкой редактирования комментария на отдельной странице задачи.\n\nEdit comment ' + this.comment.id);
     },
     onClickArchive() {
@@ -70,3 +130,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+  .nav-link {
+    font-size: 16px;
+  }
+</style>
